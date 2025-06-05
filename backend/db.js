b44df05df85953209221f2b13a32ee12549d2eba
Trevlin <152470186+loblachi@users.noreply.github.com>
2025-06-05 15:41:54 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,17 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('Trev_Survey_Db', 'root', 'Trevlin23$', {
-  host: 'localhost',
+const sequelize = new Sequelize('defaultdb', 'avnadmin', 'AVNS_0o_inS-XRLwcTTSp3iD', {
+  host: 'survey-db-survey-database.g.aivencloud.com',
+  port: 23414,
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      // Aiven requires SSL, so you enable it here:
+      require: true,
+      rejectUnauthorized: true
+    }
+  },
+  logging: false // optional: turn off SQL query logging
 });
 
 export default sequelize;
